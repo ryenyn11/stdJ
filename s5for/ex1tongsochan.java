1c99d99104b32ad18b54da4db5f4f7b5be2da851
ryenyn11 <146347486+ryenyn11@users.noreply.github.com>
2023-11-26 13:49:40 +0700

    Add files via upload

---
+++ s5for/ex1tongsochan.java
@@ -0,0 +1,14 @@
+package s5for;
+
+public class ex1tongsochan {
+
+	public static void main(String[] args) {
+		int tong=0; // tong 0-10
+		for (int i=0; i<=10; i+=2) {
+			tong+=i;//tong=tong+i 
+			System.out.println("tong bằng "+tong);
+		}
+
+	}
+
+}
---
+++ s5for/s1for.java
@@ -0,0 +1,17 @@
+package s5for;
+
+public class s1for {
+
+	public static void main(String[] args) {
+		/*
+		 for(khởi tạo; điều kiện; bước lặp){
+		   khối lệnh;
+		   }
+		 */
+		for(int i=2; i<=12; i+=2) {
+			System.out.println("i= "+i);
+		}// i chạy từ 2 tới 12, mỗi lần tăng i 2 đơ vi cho tới khi i<=12 thì kết thúc
+
+	}
+
+}
